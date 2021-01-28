908b48b1daf1db1c0f7ab9bdfc38629215b1ac28
Alexys Martinez <alexysmgjz@gmail.com>
2021-01-29 00:14:25 +0100

    Sección 5:Afiliados Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { AfiliadosComponent } from './pages/afiliados/afiliados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AfiliadosComponent
   ],
   imports: [
     BrowserModule,
