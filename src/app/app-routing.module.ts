87a566519058002aee0efd15658aa02a5abf1c43
Alexys Martinez <alexysmgjz@gmail.com>
2021-02-02 17:18:00 +0100

    nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AfiliadosComponent } from './pages/afiliados/afiliados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'afiliados', component:AfiliadosComponent},
+  {path:'**', redirectTo: 'afiliados'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/afiliados/afiliados.component.ts
+++ src/app/pages/afiliados/afiliados.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-afiliados',
   templateUrl: './afiliados.component.html',
-  styleUrls: ['./afiliados.component.css']
+  styleUrls: ['./afiliados.component.css'],
 })
-export class AfiliadosComponent implements OnInit {
+export class AfiliadosComponent {
+  afiliados: any[] = [{
+      cod: 1,
+      titulo:'gocheka',
+      categoria: 'Marquetin Digital',
+      imagen: "project-1.jpg"
+    },
+    {
+      cod: 2,
+      titulo:'Centro Medico',
+      categoria: 'Hospital',
+      imagen: "project-2.jpg"
+    },
+    {
+      cod: 3,
+      titulo:'Atlasolar',
+      categoria: 'Calentadores',
+      imagen: "project-3.jpg",
+      
+    
 
-  constructor() { }
+    },{
+      cod: 4,
+      titulo:'Zogga',
+      categoria: 'Boutique',
+      imagen: "project-4.jpg"
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor() {
+    console.log(this.afiliados);
   }
-
 }
